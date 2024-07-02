116de7f103f803e4ffc11db2432012f3ff3c65c1
Amritya Singh <amritya2021@gmail.com>
2024-07-02 12:33:11 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
-
-def get_requirements(file_path: str) -> List[str]:
-    """
-    This function will return a list of requirements from a requirements file.
-    """
-    requirements = []
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
-        
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
     return requirements
 
 setup(
-    name='ML_project',
-    version='0.0.1',
-    author='amrit',
-    author_email='amritya2021@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='amrit',
+author_email='amritya2021@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
